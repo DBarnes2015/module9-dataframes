f8d8a9885e6d8f778746d965d074747680090021
Daniel Barnes <dbarnes2@uw.edu>
2017-01-19 15:21:55 -0800

    If applied, this commit will save exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,22 +2,22 @@
 
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
-
+points <- c(12, 3, 37, 27, 26, 6, 20, 31, 31, 26, 5, 40, 10, 24, 31, 25, 26, 20)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
-
+points.allowed <- c(10, 9, 18, 17, 24, 6, 25, 25, 24, 15, 14, 7, 38, 3, 34, 23, 6, 36)
 
 # Combine your two vectors into a dataframe
-
+seahawks.points <- data.frame(points, points.allowed)
 
 # Create a new column "diff" that is the difference in points.
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
+seahawks.points$diff <- points - points.allowed
 
 # Create a new column "won" which is TRUE if the Seahawks won
-
+seahawks.points$won <- seahawks.points > 0
 
 # Create a vector of the opponent names corresponding to the games played
 
